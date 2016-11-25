e44f79e99055c8f2b6021dab22fa09fac17ab7be
Sanggyu Nam <pokeplus@gmail.com>
2016-11-25 17:47:44 +0900

    Add FeedPost class

---
+++ app/src/main/java/com/example/jaewonkim/pubsubimage/FeedPost.java
@@ -0,0 +1,13 @@
+package com.example.jaewonkim.pubsubimage;
+
+import java.net.URL;
+import java.util.UUID;
+
+/**
+ * Created by nyangkun on 13/11/2016.
+ */
+public class FeedPost {
+    public UUID id;
+    public URL imageUrl;
+    public String content;
+}
